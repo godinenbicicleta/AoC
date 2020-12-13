fb6cc069ca490620f270fb8c2f944fc45b4c7053
godinenbicicleta <carballo.bruno@gmail.com>
2020-12-12 22:46:14 -0600

    day12 in elixir

--- 2020/day12.exs
+++ 2020/day12.exs
@@ -1,21 +1,41 @@
 defmodule Day12 do
-  def read do
-    {x, y, _} =
-      File.read!("day12.txt")
-      |> String.trim()
-      |> String.split("\n", trim: true)
+  def run do
+    {{x1, y1, _}, {{x2, y2}, {_, _}}} =
+      File.stream!("day12.txt")
       |> Enum.map(&parse/1)
-      |> Enum.reduce({0, 0, "E"}, &move/2)
+      |> run()
 
-    IO.puts("p1: #{abs(x) + abs(y)}")
+    IO.puts("p1: #{abs(x1) + abs(y1)}")
+    IO.puts("p2: #{abs(x2) + abs(y2)}")
   end
 
-  def parse(string) do
-    {str, num} = String.split_at(string, 1)
-    {str, String.to_integer(num)}
+  def run(list) do
+    s = {0, 0, "E"}
+    sw = {{0, 0}, {10, 1}}
+    p1 = Enum.reduce(list, s, &move1/2)
+    p2 = Enum.reduce(list, sw, &move2/2)
+    {p1, p2}
   end
 
-  def move(a, b), do: mv(a, b) |> IO.inspect()
+  def move2(instruction, {s, w}), do: mv2(instruction, {s, w}) |> IO.inspect()
+
+  def mv2({"N", num}, {s, {x, y}}), do: {s, {x, y + num}}
+  def mv2({"S", num}, {s, {x, y}}), do: {s, {x, y - num}}
+  def mv2({"E", num}, {s, {x, y}}), do: {s, {x + num, y}}
+  def mv2({"W", num}, {s, {x, y}}), do: {s, {x - num, y}}
+  def mv2({"F", num}, {{x, y}, w = {wx, wy}}), do: {{x + num * wx, y + num * wy}, w}
+  def mv2({"L", num}, {s, w}), do: {s, rotate(w, num)}
+  def mv2({"R", num}, {s, w}), do: {s, rotate(w, 360 - num)}
+
+  def rotate({x, y}, deg) do
+    r = :math.sqrt(x * x + y * y)
+    theta = :math.atan2(y, x) + :math.pi() / 180 * deg
+    x = round(r * :math.cos(theta))
+    y = round(r * :math.cos(theta))
+    {x, y}
+  end
+
+  def move1(a, b), do: mv(a, b) |> IO.inspect()
 
   def mv({"F", num}, {x, y, "N"}), do: {x, y + num, "N"}
   def mv({"F", num}, {x, y, "S"}), do: {x, y - num, "S"}
@@ -31,4 +51,13 @@ defmodule Day12 do
   @m1 %{"N" => 90, "E" => 0, "W" => 180, "S" => 270}
   @m2 %{90 => "N", 0 => "E", 180 => "W", 270 => "S"}
   def turn(f, deg), do: @m2[rem(@m1[f] + deg, 360)]
+
+  def parse(string) do
+    {str, num} =
+      string
+      |> String.trim()
+      |> String.split_at(1)
+
+    {str, String.to_integer(num)}
+  end
 end
