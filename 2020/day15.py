3a04423c53e7c016f27fd4146b3f46bfbbe1991f
godinenbicicleta <carballo.bruno@gmail.com>
2020-12-15 16:10:50 -0600

    little bit faster

--- 2020/day15.py
+++ 2020/day15.py
@@ -4,27 +4,29 @@ from functools import partial
 
 def run(limit):
     num_turn = defaultdict(partial(deque, maxlen=2))
-    nums = [None] * (limit + 1)
     # data = [int(k) for k in "0,3,6".split(",")]
     data = [int(k) for k in "1,0,16,5,17,4".split(",")]
     print(data)
+    current = None
+    prev = None
     for turn in range(1, limit):
+        prev = current
         if data:
             num = data.pop(0)
             num_turn[num].append(turn)
-            nums[turn] = num
+            current = num
 
         else:
-            num = nums[turn - 1]
+            num = prev
             try:
                 prev_turn, last_turn = num_turn[num]
                 new_num = last_turn - prev_turn
             except ValueError:
                 new_num = 0
             num_turn[new_num].append(turn)
-            nums[turn] = new_num
+            current = new_num
 
-    print(nums[limit - 1])
+    print(current)
 
 
 if __name__ == "__main__":
