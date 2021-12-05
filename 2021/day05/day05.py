c9f0f0367e818d6f6636ee09346a58f0f546c14d
godinenbicicleta <carballo.bruno@gmail.com>
2021-12-05 13:01:58 -0600

    clean code

--- 2021/day05/day05.py
+++ 2021/day05/day05.py
@@ -1,20 +1,37 @@
 import fileinput
 from collections import defaultdict
 
-d1 = defaultdict(lambda: 0)
-part2 = []
+d1 = defaultdict(int)
+d2 = defaultdict(int)
+is_d1 = True
 for line in fileinput.input():
     in_d1 = True
     start, end = line.split(" -> ")
     start_x, start_y = [int(n) for n in start.split(",")]
     end_x, end_y = [int(n) for n in end.split(",")]
+    deltax = 1 if end_x > start_x else -1
+    deltay = 1 if end_y > start_y else -1
     if start_x != end_x and start_y != end_y:
-        part2.append(((start_x, start_y), (end_x, end_y)))
-        continue
+        for i, j in zip(
+            range(start_x, end_x + deltax, deltax),
+            range(start_y, end_y + deltay, deltay),
+        ):
+            d2[(j, i)] += 1
 
-    for i in range(min(start_x, end_x), max(start_x, end_x) + 1):
-        for j in range(min(start_y, end_y), max(start_y, end_y) + 1):
-            d1[(j, i)] += 1
+    else:
+        for i in range(start_x, end_x + deltax, deltax):
+            for j in range(start_y, end_y + deltay, deltay):
+                d1[(j, i)] += 1
+                d2[(j, i)] += 1
+
+for d in (d1, d2):
+    counter = 0
+
+    for key, value in d.items():
+        if value > 1:
+            counter += 1
+    print(counter)
+
 
 # for j in range(0, 12):
 #     for i in range(0, 12):
@@ -23,32 +40,6 @@ for line in fileinput.input():
 #         else:
 #             print(d1[(j, i)], end="")
 #     print()
-
-counter = 0
-
-for key, value in d1.items():
-    if value > 1:
-        counter += 1
-print(counter)
-
-for (start_x, start_y), (end_x, end_y) in part2:
-    if abs(end_x - start_x) != abs(end_y - start_y):
-        continue
-
-    deltax = 1 if end_x > start_x else -1
-    deltay = 1 if end_y > start_y else -1
-    for i, j in zip(
-        range(start_x, end_x + deltax, deltax),
-        range(start_y, end_y + deltay, deltay),
-    ):
-        d1[(j, i)] += 1
-
-
-counter = 0
-for key, value in d1.items():
-    if value > 1:
-        counter += 1
-print(counter)
 
 # for j in range(0, 10):
 #     for i in range(0, 10):
