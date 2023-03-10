e650b7e4e3d69f529a2106e85a776065d99b6e91
godinenbicicleta <carballo.bruno@gmail.com>
2023-03-09 18:55:09 -0600

    fix files

---
+++ 2022/aoc2022/src/main.rs
@@ -0,0 +1,54 @@
+#![feature(iter_intersperse)]
+extern crate core;
+
+// mod day01;
+// mod day02;
+// mod day03;
+// mod day04;
+// mod day05;
+// mod day06;
+// mod day07;
+// mod day08;
+// mod day09;
+// mod day10;
+// mod day11;
+// mod day12;
+// mod day13;
+// mod day14;
+// mod day15;
+//mod day16;
+//mod day17;
+//mod day20;
+// mod day18;
+mod day19;
+//mod day21;
+//mod day22;
+//mod day23;
+// mod day24;
+
+fn main() {
+    //    day01::run();
+    //    day02::run();
+    //    day03::run();
+    //    day04::run();
+    //    day05::run();
+    //    day06::run();
+    //    day07::run();
+    //    day08::run();
+    //    day09::run();
+    //    day10::run();
+    //    day11::run();
+    //day12::run();
+    //day13::run();
+    //day14::run();
+    //day15::run();
+    //   day16::run();
+    //    day17::run();
+    //day18::run();
+    day19::run();
+    //day20::run();
+    //day21::run();
+    //    day22::run();
+    //    day23::run();
+    //    day24::run();
+}
