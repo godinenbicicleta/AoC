c3548bf8fe84c777d6f096285252f09c27d0970b
godinenbicicleta <carballo.bruno@gmail.com>
2023-12-11 12:39:37 -0600

    day11 2023

--- 2023/aoc2023/lib/day11.ex
+++ 2023/aoc2023/lib/day11.ex
@@ -13,61 +13,37 @@ defmodule Day11 do
       |> List.flatten()
       |> Enum.into(%{})
 
-    galaxies = get_galaxies(grid, 2)
-    gi = galaxies |> Enum.with_index()
+    Enum.each(
+      [2, 1_000_000],
+      fn times ->
+        galaxies = get_galaxies(grid, times)
+        gi = galaxies |> Enum.with_index()
 
-    p1 =
-      for {g1, i1} <- gi, {g2, i2} <- gi, i2 > i1, reduce: 0 do
-        acc ->
-          acc + shortest(g1, g2)
+        res =
+          for {g1, i1} <- gi, {g2, i2} <- gi, i2 > i1, reduce: 0 do
+            acc ->
+              acc + shortest(g1, g2)
+          end
+
+        IO.puts("p#{times}: #{res}")
       end
-
-    IO.puts("p1: #{p1}")
-    galaxies = get_galaxies(grid, 1_000_000)
-    gi = galaxies |> Enum.with_index()
-
-    p2 =
-      for {g1, i1} <- gi, {g2, i2} <- gi, i2 > i1, reduce: 0 do
-        acc ->
-          acc + shortest(g1, g2)
-      end
-
-    IO.puts("p1: #{p2}")
+    )
   end
 
   def get_galaxies(grid, times) do
     times = times - 1
 
-    galaxies =
-      Enum.filter(grid, fn {_, v} -> v == "#" end)
-      |> Enum.map(&elem(&1, 0))
-      |> Enum.sort()
-
-    galaxies =
-      galaxies
-      |> expandx(times)
-      |> Enum.sort_by(&elem(&1, 1))
-
-    expandy(galaxies, times)
+    Enum.filter(grid, fn {_, v} -> v == "#" end)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.sort()
+    |> expandx(times)
+    |> Enum.sort_by(&elem(&1, 1))
+    |> expandy(times)
     |> Enum.sort()
   end
 
   def shortest({x1, y1}, {x2, y2}) do
     abs(x2 - x1) + abs(y2 - y1)
-  end
-
-  def diffs(g) do
-    g
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.map(fn [{x1, _y1}, {x2, _y2}] -> x2 - x1 - 1 end)
-    |> Enum.reduce([0], fn diff, [a | acc] ->
-      if diff <= 0 do
-        [a, a | acc]
-      else
-        [diff, a | acc]
-      end
-    end)
-    |> Enum.reverse()
   end
 
   def expandx(galaxies, times) do
@@ -116,14 +92,5 @@ defmodule Day11 do
     end)
     |> elem(0)
     |> Enum.reverse()
-  end
-
-  def dimensions(m) do
-    maxx = Enum.max_by(m, fn {x, _y} -> x end) |> elem(0) |> elem(0)
-    minx = Enum.min_by(m, fn {x, _y} -> x end) |> elem(0) |> elem(0)
-    maxy = Enum.max_by(m, fn {_x, y} -> y end) |> elem(0) |> elem(1)
-    miny = Enum.min_by(m, fn {_x, y} -> y end) |> elem(0) |> elem(1)
-
-    %{minx: minx, maxx: maxx, miny: miny, maxy: maxy}
   end
 end
